b3c45f902431090b9687dfb828c5d0aad27ae373
Shruti Chapadia <shruti.kaneria@gmail.com>
2015-09-17 18:11:46 -0700

    c programme

---
+++ cs204/memory allocation - malloc.c
@@ -0,0 +1,44 @@
+//
+//  memory allocation - malloc.c
+//  labwk3
+//
+//  Created by Temp on 8/21/15.
+//  Copyright (c) 2015 shruti. All rights reserved.
+//
+//
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main()Student Home | My Account | Message | Log Off
+//
+//{
+//    int arr[10];
+//    int i,count;
+//    printf("how many elements: ");
+//    scanf("%d", &count);
+//    printf("enter each element: ");
+//    for (i=0; i < count; i++)
+//    {
+//        scanf("%d", &arr[i]);
+//    }
+//    printf("the elements you entered are: ");
+//    for (i=0; i < count; i++)
+//    {
+//        printf("%d ", arr[i]);
+//    }
+//    printf("\n");
+//    printf("how many elements: ");
+//    scanf("%d", &count);
+//    printf("enter each element: ");
+//    for (i=0; i < count; i++)
+//    {
+//        scanf("%d", &arr[i]);
+//        int*arr = (int)malloc(count*sizeof(int));
+//    }
+//    printf("the elements you entered are: ");
+//    for (i=0; i < count; i++)
+//    {
+//        printf("%d ", arr[i]);
+//    }
+//    printf("\n");
+//}
---
+++ cs204/tempreture convert.c
@@ -0,0 +1,40 @@
+//
+//  tempreture convert.c
+//  labwk3
+//
+//  Created by Temp on 8/12/15.
+//  Copyright (c) 2015 shruti. All rights reserved.
+//
+
+//#include <stdio.h>
+//
+//int main (void)
+//{
+//    
+//    int fahrenheit;
+//    double celsius;
+//    
+//    printf("Enter the temperature in fahrenheit:\n");
+//    scanf("%d", &fahrenheit);
+//    celsius = (5.0/9.0) * (fahrenheit-32);
+//    printf ("The converted temperature in Celsius is:                    %    .4lf\n", celsius);
+//    
+//    return 0;
+//    
+//}
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main()
+//{
+//
+//    int a = 50;
+//    
+//    printf("Number before shift: %d\n", a);
+//    printf("Number after right shift by 4 bits: %d\n", a>>4);
+//
+//    return 0;
+//
+//
+//}
